65e41ad5bcc6b8d5931c4fd812eaa1cb7dd9ebc1
Rajshree208 <46247383+Rajshree208@users.noreply.github.com>
2020-09-04 19:48:14 +0530

    Update Program.cs

--- TelCo.ColorCoder/Program.cs
+++ TelCo.ColorCoder/Program.cs
@@ -30,19 +30,11 @@ namespace TelCo.ColorCoder
                 return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
             }
         }
-        /// <summary>
-        /// Static constructor required to initialize static variable
-        /// </summary>
         static Program()
         {
             colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
             colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
         }
-
-        /// <summary>
-        /// Test code for the class
-        /// </summary>
-        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             ColorTests();
